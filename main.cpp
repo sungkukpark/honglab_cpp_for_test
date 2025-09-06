1b4b6842f35389dfddf14e2c2103a9639c667066
Sungkuk Park <sungkukshawnpark@gmail.com>
2025-09-06 19:19:06 +0200

    add empty cpp file

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iosream>
+
+using namespace std;
+
+int main()
+{
+    cout << "hello, world" << endl;
+    return 0;
+}
+
+/*
+1. 확장(Extensions) 설치
+2. C/C++ Edit Configuration 설정
+3. Tasks 설정
+4. Launch 설정
+*/
